f555d406a74e4091e8d4edb5a3bcf4bc3ef49910
kolibri777888-sta <kolibri777888@gmail.com>
2026-03-03 10:45:34 +0300

    Day-2: Созданы экраны HomeScreen, ProfileScreen, OnBoardScreen

--- app/src/main/java/com/example/examen/data/service/UserManagementService.kt
+++ app/src/main/java/com/example/examen/data/service/UserManagementService.kt
@@ -6,6 +6,31 @@ import retrofit2.http.*
 
 const val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c3dqamR4aHN4dndwYnZ2aWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NDY2NjYsImV4cCI6MjA4ODAyMjY2Nn0.2jYBR7I2luHVGT0rFdxSSSioNv16i-flg86xcKuQFDs"
 
+data class ProfileDto(
+    val id: String?,
+    val user_id: String?,
+    val photo: String?,
+    val firstname: String?,
+    val lastname: String?,
+    val address: String?,
+    val phone: String?
+)
+
+data class FavouriteDto(
+    val id: String?,
+    val product_id: String?,
+    val user_id: String?
+)
+
+data class ProductDto(
+    val id: String,
+    val title: String,
+    val category_id: String?,
+    val cost: Double,
+    val description: String,
+    val is_best_seller: Boolean?
+)
+
 interface UserManagementService {
 
     // ---------- РЕГИСТРАЦИЯ ----------
@@ -35,4 +60,20 @@ interface UserManagementService {
         @Header("Authorization") authHeader: String,
         @Body request: ChangePasswordRequest
     ): Response<Any>
+
+    @Headers("apikey: ${com.example.examen.data.service.API_KEY}")
+    @GET("rest/v1/profiles")
+    suspend fun getProfile(
+        @Header("Authorization") authHeader: String,
+        @Query("user_id") userIdFilter: String, // "eq.<uuid>"
+        @Query("select") select: String = "*"
+    ): List<com.example.examen.data.service.ProfileDto>
+
+    @Headers("apikey: ${com.example.examen.data.service.API_KEY}", "Content-Type: application/json")
+    @PUT("rest/v1/profiles")
+    suspend fun updateProfile(
+        @Header("Authorization") authHeader: String,
+        @Query("user_id") userIdFilter: String,
+        @Body body: Map<String, Any?>
+    ): Response<Unit>
 }
