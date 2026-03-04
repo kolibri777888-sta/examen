cc3d04bafeb44b6b01c721216bdc177dbd23a0e9
kolibri777888-sta <kolibri777888@gmail.com>
2026-03-04 09:23:00 +0300

    Day-3: Добавлен экран Catalog

---
+++ app/src/main/java/com/example/examen/data/service/UserManagementService.kt
@@ -0,0 +1,110 @@
+package com.example.examen.data.service
+
+import com.example.examen.data.model.*
+import retrofit2.Response
+import retrofit2.http.PATCH
+import retrofit2.http.*
+
+const val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c3dqamR4aHN4dndwYnZ2aWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NDY2NjYsImV4cCI6MjA4ODAyMjY2Nn0.2jYBR7I2luHVGT0rFdxSSSioNv16i-flg86xcKuQFDs"
+
+data class ProfileDto(
+    val id: String?,
+    val user_id: String?,
+    val photo: String?,
+    val firstname: String?,
+    val lastname: String?,
+    val address: String?,
+    val phone: String?
+)
+data class FavouriteDto(
+    val id: String?,
+    val product_id: String?,
+    val user_id: String?
+)
+
+data class ProductDto(
+    val id: String,
+    val title: String,
+    val category_id: String?,
+    val cost: Double,
+    val description: String,
+    val is_best_seller: Boolean?
+)
+
+interface UserManagementService {
+
+    // ---------- AUTH ----------
+
+    @Headers("apikey: $API_KEY", "Content-Type: " +
+            "application/json")
+    @POST("auth/v1/signup")
+    suspend fun signUp(@Body signUpRequest: SignUpRequest): Response<SignUpResponse>
+
+    @Headers("apikey: $API_KEY", "Content-Type: application/json")
+    @POST("auth/v1/token?grant_type=password")
+    suspend fun signIn(@Body signInRequest: SignInRequest): Response<SignInResponse>
+
+    @Headers("apikey: $API_KEY", "Content-Type: application/json")
+    @POST("auth/v1/verify")
+    suspend fun verifyOTP(@Body verifyOtpRequest: VerifyOtpRequest): Response<Any>
+
+    @Headers("apikey: $API_KEY", "Content-Type: application/json")
+    @POST("auth/v1/recover")
+    suspend fun recoverPassword(@Body body: Map<String, String>): Response<Any>
+
+    @Headers("apikey: $API_KEY", "Content-Type: application/json")
+    @POST("change-password")
+    suspend fun changePassword(@Body body: ChangePasswordRequest): Response<Any>
+
+    // ---------- PROFILES ----------
+
+    @Headers("apikey:$API_KEY")
+    @GET("rest/v1/profiles")
+    suspend fun getProfile(
+        @Header("Authorization") authHeader: String,
+        @Query("user_id") userIdFilter: String,
+        @Query("select") select: String = "*"
+    ): List<ProfileDto>
+
+        @Headers("apikey: $API_KEY", "Content-Type: application/json")
+        @PATCH("rest/v1/profiles")  // Измените с PUT на PATCH
+        suspend fun updateProfile(
+            @Header("Authorization") authHeader: String,
+            @Query("user_id") userIdFilter: String,
+            @Body body: Map<String, String>
+        ): Response<Unit>
+
+    // ---------- PRODUCTS ----------
+
+    @Headers("apikey: $API_KEY")
+    @GET("rest/v1/products")
+    suspend fun getProducts(
+        @Header("Authorization") authHeader: String,
+        @Query("select") select: String = "*"
+    ): List<com.example.examen.data.service.ProductDto>
+
+    // ---------- FAVOURITE ----------
+
+    @Headers("apikey: $API_KEY")
+    @GET("rest/v1/favourite")
+    suspend fun getFavourites(
+        @Header("Authorization") authHeader: String,
+        @Query("user_id") userIdFilter: String, // "eq.<uuid>"
+        @Query("select") select: String = "id,product_id,user_id"
+    ): List<com.example.examen.data.service.FavouriteDto>
+
+    @Headers("apikey: $API_KEY", "Content-Type: application/json")
+    @POST("rest/v1/favourite")
+    suspend fun addFavourite(
+        @Header("Authorization") authHeader: String,
+        @Body body: FavouriteRequest
+    ): Response<Unit>
+
+    @Headers("apikey: $API_KEY")
+    @DELETE("rest/v1/favourite")
+    suspend fun deleteFavourite(
+        @Header("Authorization") authHeader: String,
+        @Query("user_id") userIdFilter: String, // "eq.<uuid>"
+        @Query("product_id") productIdFilter: String // "eq.<uuid>"
+    ): Response<Unit>
+    }
